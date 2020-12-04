920b9d776182eb31a3575f1b08718f5a637d8b13
BlountyNG <connor.blount@gmail.com>
2020-12-04 21:26:38 +0000

    NG Login Placeholder setup

---
+++ Dicky/Assets/NGLogin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NGLogin: MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("SceneRoomTitle");
+    }
+}
